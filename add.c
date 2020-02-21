8b1165d411c0f7af8844bad556bc43b707bbff21
MosaliDivyasri <61200598+MosaliDivyasri@users.noreply.github.com>
2020-02-21 21:10:33 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,149 @@
+#include <stdio.h>
+#include "stdint.h"
+//#include "header.h"
+//#incllude "fnctl.h"
+/*int main(void)
+{
+	uint8_t num,sum=0,ctr=0;
+	for(ctr=0;ctr<10;ctr++)
+	{
+		printf("enter the num\n");
+		scanf("%d\n",&num);
+		sum=sum+num;
+		
+	}
+	printf("%d\n",sum);
+}
+*/
+
+/*int main(void)
+{
+	uint8_t a[10]={1,2,3,4,5,6,7,8},sum=0,ctr=0,num;
+	for(ctr=0;ctr<10;ctr++)
+	{
+		//printf("enter the num\n");
+		//get(num);
+		//a[ctr]=num;
+		sum=sum+a[ctr];
+	}
+	printf("%d\n",sum);
+}*/
+
+
+/*struct st
+{
+uint8_t num;
+uint16_t num1;
+uint16_t num2;
+
+}k;
+int main(void)
+{
+	uint8_t *p;
+	p=malloc(sizeof(uint8_t)*20);
+	printf("%u\n",&k.num);
+	printf("%u\,&k.num1);
+	p=realloc(p,20);
+	printf("%u\n",&k.num2);
+	printf("%d\n",sizeof(p));
+}*/
+/*int fact(int n)
+{
+	
+	if(n)
+	return n*fact(n-1);
+	else
+		return 1;
+}
+
+uint8_t array[2],ctr;
+int main(void)
+{
+	int num=4;
+	for(ctr=0;ctr<4;ctr++)
+	{
+	array[ctr++]= fact(num);
+	}
+	for(ctr=0;ctr<4;ctr++)
+	{
+	printf("array[%d]=%d\n",ctr,array[ctr]);
+	}
+}
+*/
+
+
+/*int main(void)
+{
+	int num,ctr,pre=0,ctr1,a[5],b[5];
+	for(ctr=0;ctr<5;ctr++)
+	{
+	printf("enter the num\n");
+	scanf("%d",&num);
+	a[ctr]=num;
+	if(a[ctr]!=pre)
+	{
+		b[ctr1++]=a[ctr];
+		pre=a[ctr];
+	}
+	}
+	
+	for(ctr=0;ctr<5;ctr++)
+	{
+		printf("b[ctr]=%d",b[ctr]);
+	}
+	
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
